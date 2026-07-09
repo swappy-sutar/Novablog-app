71372e5ea405ae94cf85dbd9b44505b413a1849a
swapnil-sutar <sutarswapnil322@gmail.com>
2026-07-09 17:29:11 +0530

    style(trending): reduce Heading size to text-xs sm:text-sm font-bold tracking-wider uppercase format

--- frontend/src/components/home/TrendingTags.jsx
+++ frontend/src/components/home/TrendingTags.jsx
@@ -86,7 +86,7 @@ const TrendingTags = () => {
       {/* Trending Topics Grid */}
       <div className="max-w-7xl mx-auto px-6 space-y-4">
         <div className="flex items-center justify-between">
-          <h2 className="text-xs sm:text-sm font-bold tracking-wider text-white uppercase">
+          <h2 className="text-md sm:text-lg font-bold tracking-wider text-white uppercase">
             Trending Technical <span className="text-gradient">Topics</span>
           </h2>
           <Link 
